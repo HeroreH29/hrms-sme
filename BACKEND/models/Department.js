a4520accf7c10ba5f269f77fe4a31dfed15aad90
Hero Legaspi <139465408+HeroreH29@users.noreply.github.com>
2025-11-09 15:38:10 +0800

    Delete BACKEND directory

--- BACKEND/models/Department.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const departmentSchema = new Schema({
-  Title: String,
-});
-
-module.exports = mongoose.model("Department", departmentSchema);
--- BACKEND/models/ModeOfSeparation.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const modeOfSeparationSchema = new Schema({
-  Mode_of_Separation: String,
-});
-
-module.exports = mongoose.model("ModeOfSeparation", modeOfSeparationSchema);
